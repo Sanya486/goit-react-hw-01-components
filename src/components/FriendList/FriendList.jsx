67c4651bf36f0b5de280e4d13c0712df25dff8c2
Sanya486 <sanyok486@gmail.com>
2023-06-21 04:04:44 +0300

    Task 1-3 Done

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import css from './FriendList.module.css'
+// import PropTypes from 'prop-types';
+
+export function FriendList({friends}) {
+    return (
+      <ul className={css.friendsList}>
+        {friends.map(({ id, isOnline, avatar, name }) => {
+          return (
+            <li key={id} className={css.item}>
+              {isOnline ? (
+                <span className={css.isOnline}></span>
+              ) : (
+                <span className={css.isOffline}></span>
+              )}
+              <img className={css.avatar} src={avatar} alt={name} width={48} />
+              <p className={css.name}>{name}</p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+}
